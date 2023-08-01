c7ec749862e76c53518b03a1cb7f552fc55e7c0b
forvoi <1434326274@qq.com>
2023-08-01 17:47:47 +0800

    第二天第三次提交

---
+++ src/com/lzlgdx/day0801/Demo06.java
@@ -0,0 +1,25 @@
+package com.lzlgdx.day0801;
+
+import javax.swing.*;
+import java.util.Scanner;
+
+//演示Scanner用法
+public class Demo06 {
+    public static void main(String[] args) {
+        //界面 --JFrame类
+       /* JFrame w = new JFrame();
+        //设置界面可见  true可见 false 不可见
+        w.setVisible(true);*/
+
+        System.out.println("演示Scanner");
+        //创建Scanner类
+        Scanner sc = new Scanner(System.in);
+      /*  System.out.println("输入数据a");
+        String next = sc.next();
+        System.out.println("输出扫描结果");
+        System.out.println(next);
+        int i = sc.nextInt();*/
+       /* long phone = sc.nextLong();
+        System.out.println(phone);*/
+    }
+}
---
+++ src/com/lzlgdx/day0801/Demo07.java
@@ -0,0 +1,62 @@
+package com.lzlgdx.day0801;
+
+import java.util.Scanner;
+
+//运算符
+public class Demo07 {
+    public static void main(String[] args) {
+        //1.算术运算符
+       /* int m=5;
+        System.out.println(m/2);
+        System.out.println(m%2);
+        //定义n的取值为0-10
+        int n=0;
+        //通过计算获取输出的结果是0 1 0 1
+        System.out.println(0&2);
+        System.out.println(1&2);
+        System.out.println(2&2);
+        System.out.println(3&2);
+        System.out.println(4&2);
+        System.out.println(5&2);
+        System.out.println(6&2);
+        System.out.println(7&2);
+        System.out.println(8&2);
+        System.out.println(9&2);
+        System.out.println(10&2);*/
+
+
+       /* int n1=0;
+        //练习：定义一个三位整数，分别求个位十位百位
+        Scanner scanner = new Scanner(System.in);
+
+        int a= scanner.nextInt();
+        System.out.println(a%10+a/10%10+a/100%10);*/
+
+
+        int a=1;
+        //让a的值加1
+       /* a=a+1;
+        System.out.println(a);*/
+        int b=a++;//先使用，后自加
+        //1.先确定“a++”的值
+        //2.a自加
+        //3.将“a++”的值赋值给a;
+        System.out.println(b);
+        System.out.println(a);
+        int c=++a;//先自加，后使用
+        System.out.println(c);
+        System.out.println(a);
+
+        //面试题
+        int x=5;
+        int y=6;
+        int z=y+ --x*y++ +y;
+        System.out.println(z);
+
+        int p=3;
+        int q=p++ + ++p;
+        System.out.println(q);
+        System.out.println(p);
+
+    }
+}
