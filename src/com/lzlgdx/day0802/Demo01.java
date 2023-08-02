157cde60df7ae38d02a496d7f36476467c8efcb2
forvoi <1434326274@qq.com>
2023-08-02 10:03:04 +0800

    0802第2次提交

--- src/com/lzlgdx/day0802/Demo01.java
+++ src/com/lzlgdx/day0802/Demo01.java
@@ -3,7 +3,7 @@ package com.lzlgdx.day0802;
 public class Demo01 {
     public static void main(String[] args) {
         //逻辑与运算符 &&
-        int score = 70;
+      /*  int score = 70;
         boolean result = score > 60 && score < 90;
         System.out.println(result);
         //逻辑或运算符 ||
@@ -15,7 +15,7 @@ public class Demo01 {
         System.out.println(f1);
         System.out.println(!f1);
         System.out.println(!(age>18));
-
+*/
         //短路现象
         int i=100,j=200;
         boolean b1=(i>j) && (i++>100);//条件1为false，条件2不执行
