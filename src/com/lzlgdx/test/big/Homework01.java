5cf034832df21892a6a95583d622567f6422a744
forvoi <1434326274@qq.com>
2023-08-08 14:23:49 +0800

    0808提交大作业五子棋和猜字母

---
+++ src/com/lzlgdx/test/big/Homework01.java
@@ -0,0 +1,108 @@
+package com.lzlgdx.test.big;
+//猜字母
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+/**
+ * 要猜的字母是26个大写字母，随机生成的不重复的5个字母
+ * 提示内容：猜对了xx个字符，xx个字符的位置
+ */
+public class Homework01 {
+    /**
+     * 启动类
+     * @param args
+     */
+    public static void main(String[] args) {
+        //计算机输入一个字符数组
+        Scanner sc = new Scanner(System.in);
+        char[] inputRandom = new char[5];
+
+        System.out.println("<GuseeingGame>游戏开始,请输入你所猜的5个字母序列（exit--退出游戏）");
+        int[] result;
+        int counter=-1;
+        do {
+            char[] random;
+            //判断重复
+            do {
+                random = getChars();
+            } while (random == null);
+            String inputString = sc.next();
+            //输入五个字母或者退出游戏
+            if (inputString.equals("exit")) {
+                System.out.println("退出游戏");
+                break;
+            }
+            inputRandom = inputString.toCharArray();
+            //打印验证
+            System.out.println(Arrays.toString(random));
+            System.out.println(Arrays.toString(inputRandom));
+            //调用结果判断
+            result = getResult(random, inputRandom);
+            if (inputString.length() > 5) {
+                System.out.println("输入过多，重新输入");
+                continue;
+            }
+            //得分调用
+            counter++;
+            getOnput(result, counter);
+        } while (result[0] != result[1]||result[0]==0||result[1]==0);
+    }
+    /***
+     * 生成五个随机字母
+     * @return 字符集
+     */
+    public static char[] getChars(){
+        Random random=new Random();
+        char[] randomChars = new char[5];
+        int[] counter=new int[5];
+
+        for(int i=0;i<5;i++){
+            randomChars[i]=(char) (Math.random()*(91-65)+65);
+            counter[i]= randomChars[i];
+        }
+        Arrays.sort(counter);
+        for(int i=0;i<counter.length-1;i++){
+            if(counter[i]==counter[i+1]){
+                System.out.println("随机数有重复");
+                return null;
+            }
+        }
+
+//        System.out.println(Arrays.toString(randomChars));
+
+        return randomChars;
+    }
+    /***
+     * 提示内容：猜对了xx个字符，xx个字符的位置
+     * @param chars 输入的字符数组
+     * @return 返回字符串
+     */
+    public static int[] getResult(char[] chars,char[] chars1){
+//        int[] result = new int[2];
+        //定义两个返回值num1.num2
+        int num1=0;
+        int num2=0;
+        for (int i=0;i<chars.length;i++){
+            for(int j=0;j<chars.length;j++){
+                if(chars[i]==chars1[j]){
+                    num1++;
+                    if(i==j){
+                        num2++;
+                    }
+                }
+            }
+        }
+        int[] result={num1,num2};
+        return result;
+    }
+    public static void getOnput(int[] num,int counter){
+        //定义得分
+        int score=500-10*counter;
+        if(num[0]==5&&num[1]==5){
+            System.out.println("恭喜你猜对了！你的得分是："+score);
+        }else{
+            System.out.println("你猜对了"+num[0]+"个字符，其中"+num[1]+"个字符的位置正确！"+"已经猜了"+(counter+1)+"次(exit--退出游戏)");
+            score-=10;
+        }
+    }
+}
---
+++ src/com/lzlgdx/test/big/Homework02.java
@@ -0,0 +1,212 @@
+package com.lzlgdx.test.big;
+//五子棋游戏
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * 绘制棋盘
+ * 黑白双方落子
+ * 判断重复落子功能
+ * 判断胜利
+ */
+public class Homework02 {
+    public static void main(String[] args) {
+
+        //显示棋盘
+        char[][] chars = drawChessboard();
+        displayBoard(chars);
+        //黑方落子
+        Scanner sc = new Scanner(System.in);
+        boolean condition;
+        boolean role=false;
+        int row,col;
+        while(true) {
+            do {
+                if(!role) {
+                    System.out.println("轮到黑方落子");
+                }else{
+                    System.out.println("轮到白方落子");
+                }
+                row = sc.nextInt();
+                col = sc.nextInt();
+                if(row >=20||col >=20||row < 0||col < 0) {
+                    System.out.println("输入错误，重新输入");
+                    break;
+                }
+                condition = dropBlack(role,row, col, chars);
+                if(condition){
+                    role=!role;
+                }
+            } while (!condition);
+            //显示棋盘
+            displayBoard(chars);
+            //判断输赢
+            boolean winning = winning(row, col, chars);
+            if(winning){
+                break;
+            }
+        }
+
+    }
+
+    /**
+     * 定义棋盘
+     * @return 棋盘
+     */
+    public static char[][] drawChessboard(){
+        char[][] chessBoard=new char[20][20];
+        //定义100*100的棋盘
+        for (int i = 0; i <20;i++){
+            for (int j = 0; j <20;j++){
+                chessBoard[i][j] ='*';
+            }
+        }
+        return chessBoard;
+    }
+
+    /**
+     * 显示棋盘
+     * @param chars Character
+     */
+    public static void displayBoard(char[][] chars){
+        for(int i = 0; i <chars[1].length+1; i++){
+            System.out.print(i+"\t");
+        }
+        System.out.println();
+        for (int i = 0; i < chars.length; i++) {
+            System.out.print(i+1+"\t");
+            for (int j = 0; j < chars[i].length; j++){
+                System.out.print(chars[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * 落子
+     * @param i i
+     * @param j j
+     * @param chars chars
+     * @return true||false
+     */
+    public static boolean dropBlack(boolean role,int i, int j,char[][] chars){
+        if(chars[i-1][j-1]=='*'){
+            if(!role){
+                chars[i-1][j-1] ='@';
+            }else{
+                chars[i-1][j-1] ='O';
+            }
+            return true;
+        }
+        else{
+            System.out.println("提示：该位置已经有子,重新下子");
+            return false;
+        }
+    }
+    /**
+     * 核心算法，代码冗余，待优化
+     * @param i i
+     * @param j j
+     * @param chars chars
+     * @return true胜利 false继续落子
+     */
+    public static boolean winning(int i, int j, char[][] chars) {
+        //竖向
+        int col = 0;
+        int col1 = 0;
+        //横向
+        int row = 0;
+        int row1 = 0;
+        //左斜向
+        int left = 0;
+        int left1 = 0;
+        //右斜向
+        int right = 0;
+        int right1 = 0;
+        //竖着赢
+        for (int m = -5; m < 4; m++) {
+            if (i + m >= 0 && i + m < 20) {
+                if (chars[i + m][j - 1] == '@') {
+                    col++;
+                } else {
+                    col = 0;
+                }
+                if (chars[i + m][j - 1] == 'O') {
+                    col1++;
+                } else {
+                    col1 = 0;
+                }
+            }
+            if (col == 5 || col1 == 5) {
+                break;
+            }
+        }
+        //横着赢
+        for (int m = -5; m < 4; m++) {
+            if (j + m >= 0 && j + m < 20) {
+                if (chars[i - 1][j + m] == '@') {
+                    row++;
+                } else {
+                    row = 0;
+                }
+                if (chars[i - 1][j + m] == 'O') {
+                    row1++;
+                } else {
+                    row1 = 0;
+                }
+            }
+            if (row == 5 || row1 == 5) {
+                break;
+            }
+        }
+        //左斜着
+        for (int m = -5; m < 4; m++) {
+            if (i + m >= 0 && j + m >= 0 && i + m < 20 && j + m < 20) {
+                if (chars[i + m][j + m] == '@') {
+                    left++;
+                } else {
+                    left = 0;
+                }
+                if (chars[i + m][j + m] == 'O') {
+                    left1++;
+                } else {
+                    left1 = 0;
+                }
+            }
+            if (left == 5 || left1 == 5) {
+                break;
+            }
+        }
+        //右斜着
+        for (int m = -5; m < 4; m++) {
+            if (i + m >= 0 && j - m >= 0 && i + m < 20 && j - m < 20) {
+                {
+                    if (chars[i + m][j - m] == '@') {
+                        right++;
+                    } else {
+                        right = 0;
+                    }
+                    if (chars[i + m][j - m] == 'O') {
+                        right1++;
+                    } else {
+                        right1 = 0;
+                    }
+                }
+                if (right1 == 5 || right == 5) {
+                    break;
+                }
+            }
+        }
+            if (col == 5 || row == 5 || right == 5 || left == 5) {
+                System.out.println("黑方赢了,游戏结束");
+                return true;
+            } else if (col1 == 5 || row1 == 5 || right1 == 5 || left1 == 5) {
+                System.out.println("白方赢了,游戏结束");
+                return true;
+            } else {
+                System.out.println("继续落子！");
+                return false;
+            }
+    }
+}
